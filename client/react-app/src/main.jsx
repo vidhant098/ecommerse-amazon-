c04e5a06fe747ba078750791cf3d847010b2dab7
vidhant shriwas <vidhantshrivas1@gmail.com>
2026-03-13 22:24:14 +0530

    asjdbe

--- client/react-app/src/main.jsx
+++ client/react-app/src/main.jsx
@@ -4,7 +4,11 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <StrictMode> 
+ <BrowserRouter>
     <App />
+  </BrowserRouter>
+     
+   
   </StrictMode>,
 )
